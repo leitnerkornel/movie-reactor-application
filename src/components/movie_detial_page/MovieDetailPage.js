a3f3b564f86386263d62f3849fdcab42097dcae3
Kornél <leitner.kornel@gmail.com>
2020-05-11 23:08:39 +0200

    Place possible image sizes into a const

--- src/components/movie_detial_page/MovieDetailPage.js
+++ src/components/movie_detial_page/MovieDetailPage.js
@@ -5,8 +5,11 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_API_KEY;
 console.log(API_KEY)
 
-
-
+const imageSizes = {
+    "backdrop_sizes": ["w300", "w780", "w1280", "original"],
+    "logo_sizes": ["w45", "w92", "w154", "w185", "w300", "w500", "original"],
+    "poster_sizes": ["w92", "w154", "w185", "w342", "w500", "w780", "original"]
+};
 
 
 const MovieDetailPage = () => {
@@ -15,19 +18,20 @@ const MovieDetailPage = () => {
     const [poster, setPoster] = useState('');
 
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/495764?api_key=${API_KEY}&language=en-US`).then((res) => {
+        axios.get(`https://api.themoviedb.org/3/movie/495764?api_key=${API_KEY}`).then((res) => {
             console.log(res.data);
             setBackdrop(res.data.backdrop_path);
             setPoster(res.data.poster_path)
         });
-    },[])
+    })
 
     let content = (
         <div>
             <div className={'backdrop-container'}>
-                <img src={`https://image.tmdb.org/t/p/w780${backdrop}`} alt="Backdrop"/>
+                <img src={`https://image.tmdb.org/t/p/${imageSizes.backdrop_sizes[1]}${backdrop}`}
+                     alt="Backdrop"/>
                 <div className={'poster-container'}>
-                    <img src={`https://image.tmdb.org/t/p/w154${poster}`} alt="Poster"/>
+                    <img src={`https://image.tmdb.org/t/p/${imageSizes.poster_sizes[2]}${poster}`} alt="Poster"/>
                 </div>
             </div>
             This is a Movie Detail
