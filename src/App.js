9e95ed2fc46919395778a228a3a8512294650c48
dr. Simon Bálint <balladin0@gmail.com>
2020-05-11 19:20:55 +0200

    add burger manu skeleton

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useContext } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/layout/Menu";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
+
+  const theme = {
+    background: "none",
+    textAlign: "center",
+    padding: "10px",
+    fontSize: "1.3rem",
+    borderRadius: "15px",
+    fontFamily: " Helvetica, Arial, sans-serif",
+  };
+
+  document.title = "Movie Reactor";
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div id="outer-container">
+          <Router>
+            <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
+            <main id="page-wrap">
+              <ThemeProvider theme={theme}>
+                <Route exact path="/" component={} />
+                <Route path="/latest" component={} />
+                <Route path="/popular" component={} />
+                <Route path="/upcoming" component={} />
+              </ThemeProvider>
+            </main>
+          </Router>
+        </div>
   );
 }
 
