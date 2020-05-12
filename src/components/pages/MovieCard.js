fb596efed54353c05c35d4f768245477a0b508a0
dr. Simon Bálint <balladin0@gmail.com>
2020-05-13 00:32:25 +0200

    prepare button for unwatchlisting

--- src/components/pages/MovieCard.js
+++ src/components/pages/MovieCard.js
@@ -78,7 +78,7 @@ export default function MovieCard(props) {
     <div className="btn-group" role="group" aria-label="Basic example">
       {isTheMovieAdded() ? (
         <button type="button" className="btn btn-secondary" disabled>
-          {"Watchlisted".toUpperCase()}
+          {"unWatchlist".toUpperCase()}
         </button>
       ) : (
         <button
