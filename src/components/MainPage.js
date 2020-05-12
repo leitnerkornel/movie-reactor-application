fa5212b4fe26514a57e4e27e73ca8395bfbba1ca
PeterBorok <peter.borok@gmail.com>
2020-05-12 08:05:12 +0200

    add  blank backgroundimage style

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+const imgURL = "url()";
+
+const mainStyle = {
+  backgroundImage: imgURL,
+};
+
 export default function MainPage() {
-  return <div></div>;
+  return <div style={mainStyle}></div>;
 }
