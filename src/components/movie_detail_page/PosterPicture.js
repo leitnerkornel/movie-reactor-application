20041c82948b1e91d6ede8a0642770ae7919ca5e
Kornél <leitner.kornel@gmail.com>
2020-05-13 16:11:53 +0200

    Poster picture separated to a new element

---
+++ src/components/movie_detail_page/PosterPicture.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const PosterPicture = (props) => {
+
+    return (
+        <div className="row no-gutters">
+            <div className="col-md-12" style={{
+                height: "250px",
+                background: "rgba(52,52,52,0.8)",
+                padding: "0",
+                textAlign: "center",
+                position: "relative",
+            }}>
+                <img
+                    src={`https://image.tmdb.org/t/p/${props.imageSize}${props.poster}`}
+                    alt="Poster" style={{
+                    height: "400px",
+                    position: "absolute",
+                    top: "-190px",
+                    left: "160px",
+                    zIndex: "1",
+                    padding: "10px",
+                    backgroundColor: "#343434",
+                }}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default PosterPicture;
