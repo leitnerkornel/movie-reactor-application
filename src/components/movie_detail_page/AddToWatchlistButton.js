e7c0cdfaa3eda04d9e6f227359580d5fbf97f3a4
Kornél <leitner.kornel@gmail.com>
2020-05-14 11:55:48 +0200

    Add to watchlist button works -> need to create remove for it

--- src/components/movie_detail_page/AddToWatchlistButton.js
+++ src/components/movie_detail_page/AddToWatchlistButton.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {WatchlistContext} from "../context/WatchlistContext";
 
-const AddToWatchlistButton = () => {
-    return (
-        <div className="col-md-12 d-flex justify-content-center"
-             style={columnStyle}>
-            <button type="button" className="btn btn-warning"
-                    style={buttonStyle}>Add to Watchlist
-            </button>
-        </div>
-    );
+const AddToWatchlistButton = (props) => {
+  let movie = props.movieObject;
+  let movieId = movie.id;
 
+  const [watchlist, setWatchlist] = useContext(WatchlistContext);
+
+  let isTheMovieAdded = () => {
+    for (let selectedMovie of watchlist) {
+      if (selectedMovie.id === movieId) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  let addToWatchList = (e) => {
+    e.preventDefault();
+    if (!isTheMovieAdded() && !watchlist.includes(movie)) {
+      setWatchlist([...watchlist, movie]);
+    }
+  };
+
+  return (
+      <div className="col-md-12 d-flex justify-content-center"
+           style={columnStyle}>
+        <button
+            type="button" className="btn btn-warning"
+            style={buttonStyle}
+            onClick={addToWatchList}>
+          {"Add to Watchlist".toUpperCase()}
+        </button>
+      </div>
+  );
 }
 
 const columnStyle = {
-    height: "300px", backgroundColor: "#2e2e2e", padding: "80px 0"
+  height: "300px", backgroundColor: "#2e2e2e", padding: "80px 0"
 }
 
 const buttonStyle = {
-    fontWeight: "bold"
+  fontWeight: "bold"
 }
 
 export default AddToWatchlistButton;
