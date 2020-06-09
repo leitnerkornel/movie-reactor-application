a20167cdbaeb827e66109e561b77929bd1e56187
Kornél <leitner.kornel@gmail.com>
2020-06-09 22:04:03 +0200

    Created for constants (API key, urls, image sized etc.)

---
+++ src/Constants.js
@@ -0,0 +1,11 @@
+export const API_URL_MOVIE = "https://api.themoviedb.org/3/movie/"
+
+export const API_URL_PICTURE = "https://image.tmdb.org/t/p/"
+
+export const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const IMAGE_SIZES = {
+  backdrop_sizes: ["w300", "w780", "w1280", "original"],
+  logo_sizes: ["w45", "w92", "w154", "w185", "w300", "w500", "original"],
+  poster_sizes: ["w92", "w154", "w185", "w342", "w500", "w780", "original"],
+};
