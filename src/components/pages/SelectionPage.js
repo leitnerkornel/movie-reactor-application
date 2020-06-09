f57850e969d4338465f4ee1af02350e3f0ea608d
Kornél <leitner.kornel@gmail.com>
2020-06-09 15:47:31 +0200

    Add unique key to div inside of .map, remove comments, set dependency to useState

--- src/components/pages/SelectionPage.js
+++ src/components/pages/SelectionPage.js
@@ -1,80 +1,76 @@
-import React, {useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import Get from "../hook/FetchGet";
 import MovieCard from "./MovieCard";
 
 const SelectionPage = (props) => {
   let URL = props.url + "?api_key=" + props.API_KEY;
   const [isLoading, data] = Get(URL);
-  // const [menuOpen, setMenuOpen] = useState(false);
+
   const [barStyle, setBarStyle] = useState(36);
 
-    const [scrollY, setScrollY] = React.useState(window.pageYOffset);
+  const [scrollY, setScrollY] = React.useState(window.pageYOffset);
 
-    const getScrollY = () => {
-        setScrollY(window.pageYOffset);
-        console.log(scrollY)
-    };
+  const getScrollY = () => {
+    setScrollY(window.pageYOffset);
+  };
 
-    useEffect(() => {
-        let element = document.querySelector('#outer-container');
-        let style = getComputedStyle(element)
-        // setMenuOpen(style.perspective !== "none");
+  useEffect(() => {
+    let element = document.querySelector('#outer-container');
+    let style = getComputedStyle(element)
 
-        // setMenuOpen(style.overflow === "hidden");
-        if (style.overflow === "hidden") {
-            setBarStyle(100 + (window.innerHeight/1000)*scrollY);
-            // setBarStyle(100 + scrollY + scrollY/4);
-        } else {
-            setBarStyle(36);
-        }
+    if (style.overflow === "hidden") {
+      setBarStyle(100 + (window.innerHeight / 1000) * scrollY);
+    } else {
+      setBarStyle(36);
+    }
 
-        window.addEventListener("scroll", getScrollY);
-        return () => window.removeEventListener("scroll", getScrollY);
-    });
+    window.addEventListener("scroll", getScrollY);
+    return () => window.removeEventListener("scroll", getScrollY);
+  }, [getScrollY, scrollY]);
 
-  let layout = (
-    <React.Fragment>
-      <div className="row" style={{ backgroundColor: "#e6b31e" }}>
-        <div
-          className="col-2 align-self-start d-flex align-items-stretch"
-          style={{
-            display: "flex",
-            flexFlow: "row wrap",
-            backgroundColor: "#e6b31e",
-          }}
-        >
-          {/*<b style={{...pageTitleStyle, top: menuOpen ? 100 + scrollY + scrollY/4 : 36, transitionTimingFunction: "linear" }}>{props.title.toUpperCase()}</b>*/}
-          {/*<b style={{...pageTitleStyle, top: menuOpen ? 100 + (window.innerHeight/1000)*scrollY : 36, transitionTimingFunction: "linear" }}>{props.title.toUpperCase()}</b>*/}
-          <b style={{...pageTitleStyle, top: barStyle, transitionTimingFunction: "linear" }}>{props.title.toUpperCase()}</b>
+  return (
+      <React.Fragment>
+        <div className="row" style={{backgroundColor: "#e6b31e"}}>
+          <div
+              className="col-2 align-self-start d-flex align-items-stretch"
+              style={{
+                display: "flex",
+                flexFlow: "row wrap",
+                backgroundColor: "#e6b31e",
+              }}
+          >
+            <b style={{
+              ...pageTitleStyle,
+              top: barStyle,
+              transitionTimingFunction: "linear"
+            }}>{props.title.toUpperCase()}</b>
+          </div>
+          <div
+              className="col-10 align-self-end"
+              style={{
+                display: "flex",
+                flexFlow: "row wrap",
+                backgroundColor: "#2e2e2e",
+                marginTop: "30px",
+              }}
+          >
+            {data ? (
+                data.results.map(movie => (
+                    <div key={movie.id} className="card-deck ml-5 mr-3 mt-5">
+                      <MovieCard
+                          movie={movie}
+                          key={movie.id}
+                          API_KEY={props.API_KEY}
+                      />
+                    </div>
+                ))
+            ) : (
+                <div>Loading</div>
+            )}
+          </div>
         </div>
-        <div
-          className="col-10 align-self-end"
-          style={{
-            display: "flex",
-            flexFlow: "row wrap",
-            backgroundColor: "#2e2e2e",
-            marginTop: "30px",
-          }}
-        >
-          {data ? (
-            data.results.map((movie, index) => (
-              <div className="card-deck ml-5 mr-3 mt-5">
-                <MovieCard
-                  movie={movie}
-                  key={movie.id}
-                  API_KEY={props.API_KEY}
-                />
-              </div>
-            ))
-          ) : (
-            <div>Loading</div>
-          )}
-        </div>
-      </div>
-    </React.Fragment>
+      </React.Fragment>
   );
-
-  return layout;
 };
 
 export default SelectionPage;
@@ -82,7 +78,6 @@ export default SelectionPage;
 const pageTitleStyle = {
   position: "fixed",
   left: "12%",
-  // top: "36px",
   backgroundColor: "#e6b31e",
   zIndex: "1",
   textAlign: "left",
