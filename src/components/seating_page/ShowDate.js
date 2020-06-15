8b0ac5fe4612ee3423e98ab3e5f570523f139cec
Kornél <leitner.kornel@gmail.com>
2020-06-15 16:28:05 +0200

    Remove comments and fix typo in dateStyle variable

--- src/components/seating_page/ShowDate.js
+++ src/components/seating_page/ShowDate.js
@@ -3,22 +3,19 @@ import React from "react";
 const ShowDate = (props) => {
 
     return (
-        <div style={dateStlye}>
+        <div style={dateStyle}>
             <div className="hazy" style={{fontSize: "1.2em"}}>Date</div>
             <div style={{fontSize: "2.5em"}}>{props.date}</div>
-            {/*<div className="hazy" style={{fontSize: "0.8em"}}>Date</div>*/}
-            {/*<div style={{fontSize: "1.2em"}}>{props.date}</div>*/}
         </div>
     )
 }
 
 export default ShowDate;
 
-const dateStlye = {
-    position: "absolute",
-    top: "42%",
-    left: "38%",
-    // left: "15%",
-    zIndex: "0",
-    color: "white",
-}
+const dateStyle = {
+  position: "absolute",
+  top: "42%",
+  left: "38%",
+  zIndex: "0",
+  color: "white",
+};
