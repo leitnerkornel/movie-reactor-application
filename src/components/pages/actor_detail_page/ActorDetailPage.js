9e8ba406d2084cae2705e7d43ec33f67cb3f52cb
Kornél <leitner.kornel@gmail.com>
2020-06-25 11:16:58 +0200

    Added displayCasts component, for display all casts for actor

--- src/components/pages/actor_detail_page/ActorDetailPage.js
+++ src/components/pages/actor_detail_page/ActorDetailPage.js
@@ -3,8 +3,16 @@ import HorizontalLine from "../movie_detail_page/FirstRow";
 import {API_KEY, API_URL_PERSON, API_URL_PICTURE, IMAGE_SIZES, IMDB_ACTOR_URL} from "../../../Constants";
 
 import axios from "axios";
+import {uuid} from "uuidv4";
+import {Link} from "react-router-dom";
 import "./ActorDetailPage.css";
-import {getIdFromUrl, getAgeIfActorAlive, getAgeIfActorDead, formatDateWithDecimals} from "../../../Utils";
+import {
+  getIdFromUrl,
+  getAgeIfActorAlive,
+  getAgeIfActorDead,
+  formatDateWithDecimals,
+  getYearFromDate
+} from "../../../Utils";
 
 const ActorDetailPage = () => {
   const actorId = getIdFromUrl();
@@ -37,6 +45,7 @@ const ActorDetailPage = () => {
           res.data["cast"].map((cast) => (
               setCasts(prevState => [...prevState,
                 {
+                  movieDbId: cast["id"],
                   character: cast["character"],
                   movieTitle: cast["title"],
                   moviePoster: cast["poster_path"],
@@ -108,6 +117,44 @@ const ActorDetailPage = () => {
     }
   }
 
+  function displayCasts() {
+    let castContainer = [];
+    let castIndex = 0;
+    let numberOfRows = Math.ceil(casts.length / 2);
+    for (let i = 0; i < numberOfRows; i++) {
+      let oneRow = []
+      for (let j = 0; j < 2; j++) {
+        if (castIndex < casts.length) {
+          let imageSource = casts[castIndex]["moviePoster"] === null ? "/images/no_poster.png" : `${API_URL_PICTURE}${IMAGE_SIZES["poster_sizes"][0]}${casts[castIndex]["moviePoster"]}`
+          let movieTitleText = casts[castIndex]["movieTitle"].length > 70 ? `${casts[castIndex]["movieTitle"].substring(0, 70)}...` : casts[castIndex]["movieTitle"];
+          let characterNameText = casts[castIndex]["character"].length > 40 ? `${casts[castIndex]["character"].substring(0, 40)}...` : casts[castIndex]["character"];
+          let releaseYear = casts[castIndex]["releaseYear"] === undefined ? "" : getYearFromDate(casts[castIndex]["releaseYear"])
+          oneRow.push(<Link key={uuid()} className="link-container-cast-item"
+                            to={`/movie/${casts[castIndex]["movieDbId"]}`}>
+            <div key={uuid()} className="cast-item-container">
+
+              <div key={uuid()} className="cast-image-container">
+                <img className="cast-poster-img" src={imageSource} alt="Movie poster"/>
+              </div>
+              <div className="cast-detail-container">
+                <div className="cast-movie-title-container">{movieTitleText}</div>
+                <div className="character-name-container">{characterNameText}</div>
+                <div className="cast-movie-year-container">{releaseYear}</div>
+              </div>
+            </div>
+          </Link>);
+          castIndex++;
+        } else {
+          oneRow.push(<div key={uuid()} className="empty-item-container"/>);
+        }
+      }
+      castContainer.push(
+          <div key={uuid()} className="cast-row-container">{oneRow}</div>
+      );
+    }
+    return <div className="cast-rows-container">{castContainer}</div>;
+  }
+
   return (
       <div className={"media"}>
         <div className="col-2 align-self-start" style={{...mainColumnStyle, ...{backgroundColor: "#e6b31e"}}}>
@@ -116,7 +163,6 @@ const ActorDetailPage = () => {
           {/* The center container div. There is a grid in it. */}
           <div className="container-fluid" style={{padding: "0"}}>
             <HorizontalLine/>
-            {/*Container for cover pictures*/}
             <div className="row no-gutters" style={{backgroundColor: "#343434"}}>
               <div className="col-md-12 actor-detail-container-column">
                 <div className="actor-detail-container">
@@ -135,7 +181,6 @@ const ActorDetailPage = () => {
                         </div>
                       </div>
                     </div>
-
                     {actorBirthAndDeathInformation()}
                     <div className="actor-birth-place-imdb-container">
                       <div className="actor-birth-place-container">
@@ -165,6 +210,23 @@ const ActorDetailPage = () => {
                 </div>
               </div>
             </div>
+            <HorizontalLine/>
+            <div className="row no-gutters" style={{padding: "0"}}>
+              <div className="col-md-12 cast-title-container-column">
+                <div className="cast-title-container">
+                  <div className="cast-value">
+                    {"Casts".toUpperCase()}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row no-gutters" style={{padding: "0"}}>
+              <div className="col-md-12 cast-container-column">
+                <div className="cast-container">
+                  {displayCasts()}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-1 align-self-end" style={{...mainColumnStyle, ...{backgroundColor: "#e6b31e"}}}/>
@@ -175,7 +237,7 @@ const ActorDetailPage = () => {
 const mainColumnStyle = {
   display: "flex",
   flexFlow: "row wrap",
-  height: "1200px",
+  height: "1500px",
   padding: "0"
 }
 
