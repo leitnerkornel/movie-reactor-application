025ea79a3a7bff7e14e62a46a102c4ba086f8e40
Kornél <leitner.kornel@gmail.com>
2020-07-28 19:20:11 +0200

    Added UserProfilePage route "/profile"

--- src/App.js
+++ src/App.js
@@ -13,99 +13,98 @@ import SeatLayout from "./components/pages/SeatLayout";
 
 import "./App.css";
 import ActorDetailPage from "./components/pages/actor_detail_page/ActorDetailPage";
-// import ActorDetailPage from "./components/pages/actor_detail_page/ActorDetailPage";
+import UserProfilePage from "./components/pages/profile_page/UserProfilePage";
 
 function App() {
-
-
   document.title = "Movie Reactor";
   return (
       <div id="outer-container">
         <Router>
           <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}/>
           <WatchlistProvider>
-            <main id="page-wrap">
-              <ThemeProvider theme={theme}>
-                <div
-                    className="container-fluid"
-                    style={{maxWidth: "3000px", padding: "0"}}
-                >
-                  <Route
-                      exact
-                      path="/"
-                      render={() => (
-                          <div style={cardStyle}>
-                            <SelectionPage
-                                selection={"top_rated"}
-                                title={"Top rated movies"}
-                            />
-                          </div>
-                      )}
-                  />
-                  <Route
-                      exact
-                      path="/top_rated"
-                      render={() => (
-                          <div style={cardStyle}>
-                            <SelectionPage
-                                selection={"top_rated"}
-                                title={"Top rated movies"}
-                            />
-                          </div>
-                      )}
-                  />
-                  <Route
-                      exact
-                      path="/now_playing"
-                      render={() => (
-                          <div style={cardStyle}>
-                            <SelectionPage
-                                selection={"now_playing"}
-                                title={"Movies now playing"}
-                            />
-                          </div>
-                      )}
-                  />
-                  <Route
-                      exact
-                      path="/popular"
-                      render={() => (
-                          <div style={cardStyle}>
-                            <SelectionPage
-                                selection={"popular"}
-                                title={"Popular movies"}
-                            />
-                          </div>
-                      )}
-                  />
-                  <Route
-                      exact
-                      path="/upcoming"
-                      render={() => (
-                          <div style={cardStyle}>
-                            <SelectionPage
-                                selection={"upcoming"}
-                                title={"Upcoming movies"}
-                            />
-                          </div>
-                      )}
-                  />
-                  <Route path="/movie/:id" children={<MovieDetailPage/>}/>
-                  <Route path="/actor/:id" children={<ActorDetailPage/>}/>
-                  <Route path="/schedule" children={<SchedulePage/>}/>
-                  <Route
-                      exact
-                      path="/watchlist"
-                      render={() => (
-                          <div style={cardStyle}>
-                            <Watchlist title={"Your Watchlist"}/>
-                          </div>
-                      )}
-                  />
-                    <Route path="/reserve/:id" children={<SeatLayout/>} />
-                </div>
-              </ThemeProvider>
-            </main>
+              <main id="page-wrap">
+                <ThemeProvider theme={theme}>
+                  <div
+                      className="container-fluid"
+                      style={{maxWidth: "3000px", padding: "0"}}
+                  >
+                    <Route
+                        exact
+                        path="/"
+                        render={() => (
+                            <div style={cardStyle}>
+                              <SelectionPage
+                                  selection={"top_rated"}
+                                  title={"Top rated movies"}
+                              />
+                            </div>
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/top_rated"
+                        render={() => (
+                            <div style={cardStyle}>
+                              <SelectionPage
+                                  selection={"top_rated"}
+                                  title={"Top rated movies"}
+                              />
+                            </div>
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/now_playing"
+                        render={() => (
+                            <div style={cardStyle}>
+                              <SelectionPage
+                                  selection={"now_playing"}
+                                  title={"Movies now playing"}
+                              />
+                            </div>
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/popular"
+                        render={() => (
+                            <div style={cardStyle}>
+                              <SelectionPage
+                                  selection={"popular"}
+                                  title={"Popular movies"}
+                              />
+                            </div>
+                        )}
+                    />
+                    <Route
+                        exact
+                        path="/upcoming"
+                        render={() => (
+                            <div style={cardStyle}>
+                              <SelectionPage
+                                  selection={"upcoming"}
+                                  title={"Upcoming movies"}
+                              />
+                            </div>
+                        )}
+                    />
+                    <Route path="/movie/:id" children={<MovieDetailPage/>}/>
+                    <Route path="/actor/:id" children={<ActorDetailPage/>}/>
+                    <Route path="/schedule" children={<SchedulePage/>}/>
+                    <Route path="/profile" children={<UserProfilePage/>}/>
+                    <Route
+                        exact
+                        path="/watchlist"
+                        render={() => (
+                            <div style={cardStyle}>
+                              <Watchlist title={"Your Watchlist"}/>
+                            </div>
+                        )}
+                    />
+                    <Route path="/reserve/:id" children={<SeatLayout/>}/>
+                  </div>
+                </ThemeProvider>
+              </main>
           </WatchlistProvider>
         </Router>
       </div>
