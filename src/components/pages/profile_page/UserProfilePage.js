c883e6551110262d50e501d7594d5c3629b55299
Kornél <leitner.kornel@gmail.com>
2020-07-28 19:13:25 +0200

    UserProfilePage created with profile picture only

---
+++ src/components/pages/profile_page/UserProfilePage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import HorizontalLine from "../movie_detail_page/FirstRow";
+import "./UserProfilePage.css";
+import {uuid} from "uuidv4";
+import {Link} from "react-router-dom";
+import {API_URL_PICTURE, IMAGE_SIZES} from "../../../Constants";
+
+
+const UserProfilePage = () => {
+  let womanPicture = "woman_profile.jpg";
+  let manPicture = "man_profile.png";
+  let adminPicture = "admin_profile.png";
+
+  return (
+      <div className={"media"}>
+        <div className="col-2 align-self-start" style={{...mainColumnStyle, ...{backgroundColor: "#e6b31e"}}}>
+        </div>
+        <div className="col-9 align-self-center" style={{...mainColumnStyle, ...{backgroundColor: "#343434"}}}>
+          {/* The center container div. There is a grid in it. */}
+          <div className="container-fluid" style={{padding: "0"}}>
+            <HorizontalLine/>
+            {/*Container for cover pictures*/}
+            <div className="row no-gutters" style={{backgroundColor: "green"}}>
+              <div className="picture-name-container">
+                <div className="picture-container">
+                  <div className="profile-picture">
+                    <div className="profile-picture-frame">
+                      <img className="picture" src={`/images/${adminPicture}`} alt="Profile"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="name-container">
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            {/*Can remove or change color if it isn't fit into the look*/}
+            <HorizontalLine/>
+            <div className="row no-gutters" style={{padding: "0"}}>
+              <div className="col-md-12 schedule-container-column">
+                <div className="schedule-container">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-1 align-self-end" style={{...mainColumnStyle, ...{backgroundColor: "#e6b31e"}}}/>
+      </div>
+  );
+};
+
+const mainColumnStyle = {
+  display: "flex",
+  flexFlow: "row wrap",
+  height: "1200px",
+  padding: "0"
+}
+
+export default UserProfilePage;
